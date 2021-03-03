3c9c139e6bb4b0c4df74d7820579fb7814338ae8
Songtech-0912 <jacky.song1020@gmail.com>
2021-03-03 10:02:46 -0500

    Modified build scripts to not build for onefile and clear pycache

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ echo -e "Starting build..."
 pip install pyinstaller
 pip install pywebview
 pip install --user PyGobject
-pyinstaller --add-data 'index.html:.' --add-data 'output.style.css:.' --onefile Randomizer.py
+# pyinstaller --add-data 'index.html:.' --add-data 'output.style.css:.' --onefile Randomizer.py
+pyinstaller --add-data 'index.html:.' --add-data 'output.style.css:.' Randomizer.py
 echo -e "Build finished. Executable in ./dist/Randomizer"
