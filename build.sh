8e6a2e9d67f72b6954b247dea04e6cabfcf96852
Songtech-0912 <jacky.song1020@gmail.com>
2021-03-04 20:12:32 -0500

    Tree shake dead modules in build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ pip install pyinstaller
 pip install pywebview
 pip install --user PyGobject
 # pyinstaller --add-data 'index.html:.' --add-data 'output.style.css:.' --onefile Randomizer.py
-pyinstaller --add-data 'index.html:.' --add-data 'output.style.css:.' Randomizer.py
+pyinstaller --add-data 'index.html:.' --add-data 'output.style.css:.' --exclude-module PyQt5 --exclude-module sympy --exclude-module numpy Randomizer.py
 echo -e "Build finished. Executable in ./dist/Randomizer"
