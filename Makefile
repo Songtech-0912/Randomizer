e444ea234481d27cf5259970f6d6687f8158cb4b
Songtech-0912 <jacky.song1020@gmail.com>
2021-03-05 01:13:28 -0500

    Created appimage build scripts

--- Makefile
+++ Makefile
@@ -6,3 +6,9 @@ clean:
 
 debug:
 	bash debug.sh
+
+appimage:
+	bash build-appimage.sh
+
+macapp:
+	bash build-macos-app.sh
---
+++ build-appimage.sh
@@ -0,0 +1,15 @@
+echo -e "Generating AppImage..."
+# We copy the generate binary from dist/
+cp ./dist/Randomizer ./release/appimage-build/Randomizer-Linux_64.AppDir/usr/bin/
+# Then download appimagetool for generating appimages
+# Here we fallback to curl if wget is not available
+mkdir cache
+if command -v "$pip3" > /dev/null; then
+    # Use curl to download
+    curl -O "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" ./cache
+fi
+wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" ./cache
+# Now we run appimagetool
+chmod a+x ./cache/appimagetool-x86_64.AppImage
+./cache/appimagetool-x86_64.AppImage ./release/appimage-build/Randomizer-Linux_64.AppDir
+echo -e "AppImage successfully built at ./release/appimage-build!"
