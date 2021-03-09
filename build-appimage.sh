1ec91bfb2f9e75fa3faa0d62b6cf9e264ce1b7db
Songtech-0912 <jacky.song1020@gmail.com>
2021-03-08 21:15:52 -0500

    Changed path entry in AppImage build script

--- build-appimage.sh
+++ build-appimage.sh
@@ -4,6 +4,8 @@ cp ./dist/Randomizer ./release/appimage-build/Randomizer-Linux_64.AppDir/usr/bin
 # Then download appimagetool for generating appimages
 # Here we fallback to curl if wget is not available
 mkdir cache
+mkdir /release/appimage-build/Randomizer-Linux_64.AppDir/usr
+mkdir /release/appimage-build/Randomizer-Linux_64.AppDir/usr/bin
 if command -v "$pip3" > /dev/null; then
     # Use curl to download
     curl -O "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" ./cache
